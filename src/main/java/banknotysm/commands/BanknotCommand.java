8d6fef9044092eb56563590ddb27dac25ed5d68f
luki3122 <luki3122@gmail.com>
2020-08-20 22:19:40 +0200

    Dodano event i refaktoryzacja kodu

--- src/main/java/banknotysm/commands/BanknotCommand.java
+++ src/main/java/banknotysm/commands/BanknotCommand.java
@@ -1,8 +1,11 @@
+﻿
 package banknotysm.commands;
 
 import banknotysm.BanknotySM;
 import banknotysm.configs.Config;
 import banknotysm.util.BanknotyUtil;
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -11,8 +14,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class BanknotCommand implements CommandExecutor {
 
@@ -22,85 +27,94 @@ public class BanknotCommand implements CommandExecutor {
         Config config = BanknotySM.getInternalConfig();
         Player p = (Player) sender;
         PlayerInventory inv = p.getInventory();
-        if (args.length >= 1) {
-// /banknot kup 1
-            if(inv.firstEmpty() >0) {
-                if (args.length == 2) {
-                    if (args[1].equalsIgnoreCase("1") || args[1].equalsIgnoreCase("2") || args[1].equalsIgnoreCase("5") ||
-                            args[1].equalsIgnoreCase("10") || args[1].equalsIgnoreCase("20") || args[1].equalsIgnoreCase("50") || args[1].equalsIgnoreCase("100")) {
-                        if (p.hasPermission(BanknotyUtil.BANKNOT_GIVE_SELF_PERM)) {
 
-                            ItemStack banknot = BanknotyUtil.createBanknot(Integer.parseInt(args[1]));
-                            if (args[0].equalsIgnoreCase("kup")) {
-                                if (BanknotySM.getEconomy().getBalance(p) >= Integer.parseInt(args[1])) {
-                                    inv.addItem(banknot);
-                                    BanknotySM.getEconomy().withdrawPlayer(p, Integer.parseInt(args[1]));
-                                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getBuyBanknotMessage()).replaceAll("%PLAYER%", p.getDisplayName()));
-                                } else {
-                                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getNoMoneyMessage()));
-                                }
-                            }
-                            //Need a change - need a method to compare ItemStack with customitem - equals don't work, isSimillar same
-                        } else {
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getNoPermMessage()));
+        if (args.length == 0) {
+            p.sendMessage(ChatColor.AQUA + "Poprawne uzycie: /banknoty kup/sprzedaj (liczba)");
+            return false;
+        } //err brak argumentów
 
-                        }
-                    }
-                } else if (args[0].equalsIgnoreCase("sprzedaj")) {
-                    String lore = p.getInventory().getItemInMainHand().getItemMeta().getLore().get(0);
-                    if (p.getInventory().getItemInMainHand().getType() != Material.AIR && lore != null) {
-                        String mess = ChatColor.translateAlternateColorCodes('&', config.getSellBanknotMessage()).replaceAll("%PLAYER%", p.getDisplayName());
-                        ItemStack item = p.getInventory().getItemInMainHand();
-                        int howmany = item.getAmount();
-                        if (lore.equals("Banknot o wartości 1$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 2$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 2 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 5$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 5 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 10$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 10 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 20$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 20 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 50$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 50 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else if (lore.equals("Banknot o wartości 100$")) {
-                            inv.removeItem(item);
-                            BanknotySM.getEconomy().depositPlayer(p, 100 * howmany);
-                            p.sendMessage(ChatColor.AQUA + mess);
-                        } else {
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getNoBanknotInHandMessage()));
-                        }
-                    }
-                } else if (args[0].equalsIgnoreCase("reload")) {
-                    if (p.hasPermission(BanknotyUtil.BANKNOT_RELOAD_PERM)) {
-                        config.reloadConfig();
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getReloadMessage()));
 
-                        return true;
-                    }
-                }
-            }
-            else{
-                p.sendMessage(ChatColor.RED+"Nie masz wolnego miejsca w ekwipunku!");
-            }
+        if (args[0].equalsIgnoreCase("kup")) {
+            if (!p.hasPermission(BanknotyUtil.BANKNOT_GIVE_SELF_PERM)) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.RED+ config.getNoPermMessage()));
+                return false;
+            } //err brak permisji
+
+            if (args.length < 2) {
+                p.sendMessage(ChatColor.RED + "Podaj cenę!");
+                return false;
+            } // err brak podanej ceny
+
+            if (inv.firstEmpty() == -1) {
+                p.sendMessage(ChatColor.RED + "Nie masz wolnego miejsca w ekwipunku!");
+                return false;
+            } //err no inv space
+
+            int price = 0;
+
+            try {
+                price = Integer.parseInt(args[1]);
+                if(price < 1) throw new Exception();
+            } catch (Exception ee) {
+                p.sendMessage(ChatColor.RED + "Podaj dodatnią liczbę jako cenę!");
+                return false;
+            } //err nie numer jako argument
+
+            if (BanknotySM.getEconomy().getBalance(p) < price) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.RED + config.getNoMoneyMessage()));
+                return false;
+            } //err nie stać
+
+            ItemStack banknot = BanknotyUtil.createBanknot(price);
+            inv.addItem(banknot);
+            BanknotySM.getEconomy().withdrawPlayer(p, price);
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getBuyBanknotMessage()).replaceAll("%PLAYER%", p.getDisplayName()));
+            return true;
         }
-            else{
-            p.sendMessage(ChatColor.AQUA+"Poprawne uzycie: /banknoty kup/sprzedaj (1,2,5,10,20,50,100)");
 
-        } return false;
+        if (args[0].equalsIgnoreCase("sprzedaj")) {
+            String lore = "";
+            try{
+                 lore = Objects.requireNonNull(p.getInventory().getItemInMainHand().getItemMeta().getLore()).get(0);
+                if(!p.getInventory().getItemInMainHand().getType().toString().equals("PAPER")) throw new Exception();
+            } catch (Exception e){
+
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.RED + config.getNoBanknotInHandMessage()));
+                return false;
+            } //err item has no lore
+
+
+            String mess = ChatColor.translateAlternateColorCodes('&', config.getSellBanknotMessage()).replaceAll("%PLAYER%", p.getDisplayName());
+            ItemStack item = p.getInventory().getItemInMainHand();
+            int howmany = item.getAmount();
+            int price = 0;
+
+            try {
+                price = Integer.parseInt(lore.replaceAll("\\D+", ""));
+            } catch (Exception e) {
+                p.sendMessage(ChatColor.RED + "Twój banknot nic nie kosztował??");
+            }  //err nigdy nie powinien się wywołać
+
+            inv.removeItem(item);
+            BanknotySM.getEconomy().depositPlayer(p, howmany * price);
+            p.sendMessage(ChatColor.AQUA + mess);
+            return true;
+
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!p.hasPermission(BanknotyUtil.BANKNOT_RELOAD_PERM)) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.RED + config.getNoPermMessage()));
+                return false;
+            } //err brak permisji
+
+            config.reloadConfig();
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.AQUA + config.getReloadMessage()));
+
+            return true;
+        }
+
+        p.sendMessage(ChatColor.AQUA + "Poprawne uzycie: /banknoty kup/sprzedaj (liczba)");
+        return false;
     }
 }
 
